1a9288758ff1feb5d93b065e32f39efed901a80a
Zooted <101065235+fantae2226@users.noreply.github.com>
2026-05-07 13:11:50 -0400

    init commit

---
+++ pokemon-comp/src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.css';
+export default function Header() {
+    return (
+        <header>
+            <h1>Pokémon Compendium</h1>
+            <div className="background">
+                
+            </div>
+        </header>
+    );
+}
---
+++ pokemon-comp/src/components/LoginSignup/LoginSignup.tsx
@@ -0,0 +1,10 @@
+import './LoginSignup.css';
+export default function LoginSignup() {
+    return (
+        <div className="login-signup-container">
+            <div className="background">
+                
+            </div>
+        </div>
+    );
+}
